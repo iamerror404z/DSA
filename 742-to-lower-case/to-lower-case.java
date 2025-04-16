584301ac573fa4c2928f37bdb7343f7411154461
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-04-16 09:41:36 +0530

    Time: 6 ms (7.38%) | Memory: 42.4 MB (5.82%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,7 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
-        String temp=s.toLowerCase();
-        return temp;
+        String temp="";
+        for (int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='A' && ch<='Z' ){
+                temp+=(char)(ch+32);
+            }else{
+                temp+=ch;
+            }
+        }return temp;
         
     }
 }
