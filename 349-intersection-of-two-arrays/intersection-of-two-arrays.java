6b8b835e93276373ece4581ae915562b3eb7d572
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-04-22 20:31:25 +0530

    Time: 8 ms (6.40%) | Memory: 43.7 MB (14.67%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         int[] freq=new int[1001];
+        int[] temp=new int[1001];
         int max=-1;
         // int max= Math.max(nums1.length,nums2.length);
         for (int i:nums1){
@@ -9,15 +10,13 @@ class Solution {
         int pos=0;
         for (int j:nums2){
             // System.out.println(j+" "+freq[j]);
-            if(freq[j]>0 && freq[j]!=2){
-                if(max<j){max=j;}
-                freq[j]=2;pos++;}}
+            if(freq[j]>0){
+                temp[pos++]=j;
+                    --freq[j];}}
         int[] ans=new int[pos];
         int pt=0;
-        for(int l=0;l<=max;l++){
-            System.out.println(freq[l]+" "+l);
-            if(freq[l]==2){ans[pt++]=l;}
-        }
+        for(int l=0;l<pos;l++){
+            ans[pt++]=temp[l];}
         System.out.println(Arrays.toString(ans)+"  "+pos);
         return ans;
 }}
