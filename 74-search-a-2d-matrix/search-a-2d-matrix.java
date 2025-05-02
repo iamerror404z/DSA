8883d1fd3c6ecad463c7a3fe6fd3717905552f61
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-05-02 07:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.82%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int[] i:matrix){
+            for(int j:i){
+                if(j==target){
+                    return true;}
+            }
+        }
+        return false;
+    }
+}
