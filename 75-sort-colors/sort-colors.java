95576e80cb61bc6dd444d0ba4598230639948969
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-05-18 21:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (9.55%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        // using the dutch nationa flag alogorithim to solve this, refer the note under alog section 
+
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
     }
 }
