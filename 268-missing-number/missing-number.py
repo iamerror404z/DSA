3ffa117cb9a3c4a2a751d64434b82ffaf32f6edd
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-05-29 19:54:51 +0530

    Time: 3 ms (63.72%) | Memory: 18.7 MB (51.71%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,11 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        point=0
-        while(point<len(nums)):
-            if(point!=nums[point]):
-                return point
-            point+=1
-        
-        return point
+        max=-1
+        sum=0
+        low=-1
+        for i in nums:
+            if(i>max):
+                max=i
+            if (i==0):
+                low=0
+            sum+=i
+        math=max*(max+1)//2-sum
+        print(math)
+        if(math==0 and low==0):
+            return max+1
+
+        return math
         
