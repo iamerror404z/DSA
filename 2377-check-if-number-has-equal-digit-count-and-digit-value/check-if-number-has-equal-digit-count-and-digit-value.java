456e66dc6652dbc38fcc29d55f20b63a5e65ed1d
pravin747z <praveenkumar.m2004pk@gmail.com>
2025-05-31 11:37:30 +0530

    Time: 1 ms (63.52%) | Memory: 41.1 MB (100.00%) - LeetSync

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean digitCount(String num) {
-        int[] freq=new int[10];
-        // HashMap<Character,Integer> freq=new HashMap<>();
-        for(int i=0;i<num.length();i++){
-            // System.out.println(num.charAt(i));
-            int value=Integer.valueOf(""+num.charAt(i));
-            System.out.println(value);
-            freq[value]=freq[value]+1;
-            
+       int[] freq=new int[10];
+       for(int i=0;i<num.length();i++){
+        freq[num.charAt(i)-'0']++;
+       } 
+       for(int i=0;i<num.length();i++){
+        int comp=num.charAt(i)-'0';
+        // System.out.println(comp+" comp"+freq[i]);
+        if(comp!=freq[i]){
+            return false;
         }
-        for(int i=0;i<num.length();i++){
-            int check=Integer.valueOf(""+num.charAt(i));
-            if(freq[i]!=check){
-                return false;
-            }
-        }
+       }
 
-        return true;
+       return true;
     }
 }
