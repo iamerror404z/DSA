72190ee407264daf362e4dd25e81e3db909debf5
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-06-07 08:57:12 +0530

    Time: 3 ms (46.96%) | Memory: 41.8 MB (41.26%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time=0;
+        int[] freq=new int[tickets.length];
+
+        while(freq[k]!=tickets[k]){
+            for(int i=0;i<tickets.length;i++){
+                if(tickets[i]!=freq[i]){
+                    freq[i]++;
+                    time++;
+                }
+                if(freq[k]==tickets[k]){
+                    return time;
+                }
+            }
+        }
+        
+
+        return -1;
+    }
+}
