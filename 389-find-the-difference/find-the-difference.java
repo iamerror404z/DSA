1076eceac09eea94555b335cb810c561ca8a6146
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-06-08 17:07:52 +0530

    Time: 3 ms (41.55%) | Memory: 42 MB (47.58%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            freq[ch-'a']++;
+        }
+    
+    for(int i=0;i<t.length();i++){
+        char ch=t.charAt(i);
+        freq[ch-'a']--;
+        if(freq[ch-'a']<=-1) return ch;
+    }
+    
+
+        return 'a';
+    }
+}
