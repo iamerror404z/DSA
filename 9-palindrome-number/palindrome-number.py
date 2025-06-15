00eaab230a36911c3b792b1d46658a7bb3daf61f
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-06-15 13:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.76%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def isPalindrome(self, x):
         y=str(x)
-        z=""
-        for i in range(-1,-len(y)-1,-1):
-            z+=y[i]
-        return (str(x)==(z))
+        return y[::-1]==y
 
         
