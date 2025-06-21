32ad94f438a01a951617ed143420a787635c2735
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-06-21 18:36:44 +0530

    Time: 4 ms (19.83%) | Memory: 44 MB (45.45%) - LeetSync

--- 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.java
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.java
@@ -2,6 +2,13 @@ class Solution {
     public int findKOr(int[] nums, int k) {
         int size=nums.length;
         StringBuilder sb=new StringBuilder();
+        int max=nums[0];
+        for (int i:nums){
+            if(i>max){
+                max=i;
+            }
+        }
+        int limit=Integer.toBinaryString(max).length();
         for (int i=31;i>=0;i--){
             int repeat=0;
             for (int num:nums){
