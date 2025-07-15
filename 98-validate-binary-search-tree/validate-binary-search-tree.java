f5e848a2d6916ff971f6f21f81106ea9563acdcf
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-07-15 22:53:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (78.34%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,8 +14,6 @@
  * }
  */
 class Solution {
-
-    List<Integer> lst=new LinkedList<Integer>();
     int step=0;
     int prev=0;
     boolean ans=true;
@@ -27,13 +25,9 @@ class Solution {
         inOrder(curr.left);
         if(step!=0&&prev>=curr.val){
             ans=false;
-            // System.out.println(curr.val+"    "+prev);
-            System.out.println(prev>curr.val);
-            System.out.println(curr.val);
         }
         prev=curr.val;
         step++;
-        lst.add(curr.val);
         inOrder(curr.right);
         }
 
@@ -42,11 +36,6 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         inOrder(root);
         
-        // for(int i=1;i<lst.size();i++){
-        //     if(lst.get(i)<=lst.get(i-1)){
-        //         return false;
-        //     }
-        // }
         
         return ans;
         
