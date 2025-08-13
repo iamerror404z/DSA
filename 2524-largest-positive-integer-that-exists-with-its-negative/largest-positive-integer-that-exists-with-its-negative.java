1d65bd2b6b78bfd1bb15385ee4bc9777f2c92507
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-08-13 19:56:00 +0530

    Time: 5 ms (85.71%) | Memory: 45 MB (65.82%) - LeetSync

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxK(int[] nums) {
+     Arrays.sort(nums);
+     int left=0;
+     int len=nums.length;
+     int right=len-1;
+     int num=nums[left];
+     while(left<len && right>=0){
+        if(-1*nums[left]==nums[right]){
+            return nums[right];
+        }else if(-1*nums[left]>nums[right]){
+            left++;
+        }else{
+            right--;
+        }
+     }
+
+    return -1;
+    }
+}
