be7f47f29fe94825e9f761eb34598fe931aee65f
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-08-16 14:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (15.69%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximum69Number (int num) {
+        int rev=0;
+        int changes=0;
+        int b=num;
+        while(b!=0){
+            rev+=(b%10);
+            rev*=10;
+            b/=10;
+        }
+        b=rev/10;
+        int res=0;
+        int  d=0;
+        while(b!=0){
+            d=(b%10);
+            if(d==6 && changes==0){
+                res+=9;
+                changes++;
+            }else{
+                res+=d;
+
+            }
+            res*=10;
+            b/=10;
+        }
+
+       
+       return res/10;
+    }
+}
