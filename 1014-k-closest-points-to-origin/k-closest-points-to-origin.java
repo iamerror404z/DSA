1eba251af24b350da9f67209e2040b035dfd7c71
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-10-23 22:01:56 +0530

    Time: 262 ms (5.02%) | Memory: 55.9 MB (10.22%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,51 @@
+class Solution {
+    Map<Integer,List<Integer>> track=new HashMap<>(); 
+
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] ans=new int[k][2];
+        List<Integer> keys=new LinkedList<>();
+
+        int required=k;
+
+        for(int i=0;i<points.length;i++){
+            int[] temp=points[i];
+            int distance=(temp[0]*temp[0])+(temp[1]*temp[1]);
+
+            if(track.containsKey(distance)){
+                track.get(distance).add(i);
+            }else{
+                keys.add(distance);
+                List<Integer> lister=new LinkedList<>();
+                lister.add(i);
+                track.put(distance,lister);
+
+            }
+        }
+
+        Collections.sort(keys);
+        int pointer=0;
+
+        for(int i:keys){
+            int size=track.get(i).size();
+            size=Math.min(size,required);
+            List<Integer> temp=track.get(i);
+
+            for(int j=0;j<size;j++){
+                ans[pointer++]=points[temp.get(j)];
+                System.out.println(Arrays.toString(points[temp.get(j)]));
+            }
+
+            
+            required-=size;
+            if(required<=0){
+                break;
+            }
+        }
+
+
+
+
+
+        return ans;
+    }
+}
