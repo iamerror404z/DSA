60fea26149c132330219f67817c8d953dc38a9d2
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-10-26 22:53:20 +0530

    Time: 155 ms (5.13%) | Memory: 103.4 MB (10.94%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -0,0 +1,76 @@
+class Bank {
+    long[] balance;
+    int size;
+
+    public Bank(long[] balance) {
+        this.balance=balance;
+        size=balance.length;
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        if(account1>size || account2>size){
+            return false;
+        }
+
+        if(account1<1 || account2<1){
+            return false;
+        }
+
+
+        if(money<0){
+            return false;
+        }
+
+        
+
+        if(balance[account1-1]<money){
+            return false;
+        }
+
+        balance[account1-1]-=money;
+        balance[account2-1]+=money;
+
+
+        return true;
+
+    }
+    
+    public boolean deposit(int account, long money) {
+        System.out.println("Account , money "+account+" , "+money);
+        
+        if(account<1 || account>size){
+            return false;
+        }
+        if(money<0){
+            return false;
+        }
+
+        balance[account-1]+=money;
+
+
+        return true;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(account<1 || account>size){
+            return false;
+        }
+
+        if(money<0 || balance[account-1]<money){
+            return false;
+        }
+
+
+        balance[account-1]-=money;
+
+        return true;
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
