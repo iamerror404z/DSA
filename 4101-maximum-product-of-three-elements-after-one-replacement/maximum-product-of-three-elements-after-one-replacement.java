6d96a9b7f0a09b56321ec6216d83d9834a649a5e
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-11-02 21:07:41 +0530

    Time: 52 ms (100.00%) | Memory: 99.8 MB (0.00%) - LeetSync

---
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long maxProduct(int[] nums) {
+        int size=nums.length;
+        int[] temp=new int[size];
+
+        for(int i=0;i<size;i++){
+            temp[i]=Math.abs(nums[i]);
+        }
+
+        Arrays.sort(temp);
+
+        int min=temp[size-1];
+        int pos=size-1;
+        for(int i=size-3;i<size;i++){
+            if(temp[i]<min){
+                min=temp[i];
+                pos=i;
+            }
+        }
+
+        long product=1;
+
+        for(int i=size-3;i<size;i++){
+            if(i!=pos){
+                product*=(long)temp[i];
+            }
+        }
+        
+        long max=Long.MAX_VALUE;
+        long div=0;
+
+        if(product!=0){
+         div=max/product;}
+        long choosen=Math.min(div,(long)Math.pow(10,5));
+
+        product*=choosen;
+        System.out.println(choosen);
+
+        return  product;
+    }
+}
