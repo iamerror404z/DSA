91e7453378a152ade60ef0ba610f89a752ea9f50
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-11-08 20:48:32 +0530

    Time: 17 ms (6.44%) | Memory: 46.9 MB (6.11%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> answer(int[] arr){
+        List<List<Integer>> outer=new LinkedList<>();
+        int start=0;
+        int end=0;
+        outer.add(new LinkedList<>());
+
+        for(int  i=0;i<arr.length;i++){
+            start=0;
+            if(i>0 && arr[i]==arr[i-1]){
+                System.out.println(end+1);
+                start=end;
+            }
+
+            end=outer.size();
+            for(int j=start;j<end;j++){
+                List<Integer> inner=new LinkedList<>(outer.get(j));
+                inner.add(arr[i]);
+                System.out.println(inner);
+                outer.add(inner);
+            }
+        
+        }
+
+
+
+        return outer;
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        
+
+        return answer(nums);
+    }
+}
