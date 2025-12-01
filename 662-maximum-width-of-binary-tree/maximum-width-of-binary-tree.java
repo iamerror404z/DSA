449b170854546f13de21770de58014f1450dae20
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-01 15:36:50 +0530

    Time: 3 ms (5.53%) | Memory: 45.6 MB (5.40%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,75 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-
-    int level=0;
-    long[][] size;
-
-    public void depth(TreeNode curr,int Level){
-        if(curr==null){
-            level=Math.max(level,Level-1);
-            return;
-        }
-
-        depth(curr.left,Level+1);
-        depth(curr.right,Level+1);
-
-    }
-
-
-    public  void width(TreeNode curr,int level,long pos){
-        if(curr==null){
-            return;
-        }
-            
-            size[level][0]=Math.min(size[level][0],pos);
-            size[level][1]=Math.max(size[level][1],pos);
-
-        
-
-        width(curr.left,level+1,((pos-1)*2)+1);
-        width(curr.right,level+1,((pos-1)*2)+2);
-
-        
-
-    }
-
-
     public int widthOfBinaryTree(TreeNode root) {
-        depth(root ,1);
-        size=new long[level][2];
-        
-
-        for(int i=0;i<level;i++){
-            size[i][0]=Long.MAX_VALUE;
-            size[i][1]=Long.MIN_VALUE;
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Map<TreeNode, Integer> m = new HashMap<TreeNode, Integer>();
+        q.offer(root);
+        m.put(root, 1);
+        int curW = 0;
+        int maxW = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            int start = 0;
+            int end = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                if(i == 0) start = m.get(node);
+                if(i == size - 1) end = m.get(node);
+                if(node.left != null){
+                    m.put(node.left, m.get(node) * 2);
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    m.put(node.right, m.get(node) * 2 + 1);
+                    q.offer(node.right);
+                }
+            }
+            curW = end - start + 1;
+            maxW = Math.max(curW, maxW);
         }
-
-        
-
-        width(root,0,1);
-       
-        long ans=1;
-
-        for(long[] i:size){
-            ans=Math.max(ans,i[1]-i[0]+1);
-        }
-
-        
-        return (int)ans;
-
+        return maxW;
     }
 }
