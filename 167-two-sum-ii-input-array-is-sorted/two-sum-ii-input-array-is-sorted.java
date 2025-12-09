de19f636376a4172ca03c11a274fe753fce9d037
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-09 19:56:16 +0530

    Time: 4 ms (17.55%) | Memory: 48.3 MB (34.60%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,41 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] ans=new int[2];
-        int n=numbers.length;
-        for (int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(numbers[i]+numbers[j]==target){
-                    ans[0]=i+1;
-                    ans[1]=j+1;
-                    return ans;
-                }
+
+    public int binSearch(int[] arr,int target,int start,int end){
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
+
+
+        return -1;
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int[] pair=new int[2];
+        int n=numbers.length;
+
+        for(int i=0;i<n;i++){
+            int pos=binSearch(numbers,target-numbers[i],i+1,n-1);
+            if(pos!=-1){
+                pair[0]=i+1;
+                pair[1]=pos+1;
+
+                return pair;
+            }
+        }
+
+        
     
-        return ans;
+        return pair;
     }
 }
