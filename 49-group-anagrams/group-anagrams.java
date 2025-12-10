e40ff669a0a740ab9396e0d86a047931f17db78f
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-10 08:21:49 +0530

    Time: 14 ms (30.67%) | Memory: 50.9 MB (7.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,10 +3,14 @@ class Solution {
         HashMap<ArrayList<Integer>,ArrayList<String>> group =new HashMap<>();
 
         for(String s:strs){
+
+
             ArrayList<Integer> key=new ArrayList<>(Collections.nCopies(26,0));
             for(int i=0;i<s.length();i++){
                 char ch=s.charAt(i);
                 key.set(ch-'a',key.get(ch-'a')+1);
+
+
             }
             if(group.containsKey(key)){
             group.get(key).add(s);}
@@ -16,6 +20,8 @@ class Solution {
                 group.put(key,gg);
             }
         }
+
+        
 
             List<List<String>> ans=new ArrayList<>();
             for(ArrayList<Integer> ls:group.keySet()){
