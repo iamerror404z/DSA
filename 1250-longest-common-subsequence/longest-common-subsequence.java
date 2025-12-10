995f557fc217425bc4a50498fb0c71a58d8c7deb
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-10 10:38:53 +0530

    Time: 20 ms (75.68%) | Memory: 54.1 MB (19.21%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,44 +1,28 @@
 class Solution {
-
-    public int LCS(int index1,int index2,int[][] dp,String t1,String t2){
-        if(index1<=-1 || index2<=-1){
-            return 0;
-        }
-        
-        if(dp[index1][index2]!=-1){
-            return dp[index1][index2];
-        }
-
-        if(t1.charAt(index1)==t2.charAt(index2)){
-            return dp[index1][index2]=1+LCS(index1-1,index2-1,dp,t1,t2);
-        }
-
-        
-
-        return dp[index1][index2]=Math.max(LCS(index1,index2-1,dp,t1,t2),LCS(index1-1,index2,dp,t1,t2));
-
-    }
-
     public int longestCommonSubsequence(String text1, String text2) {
-        int m=text1.length();
-        int n=text2.length();
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
+      int m=text1.length();
+      int n=text2.length();
+      int[][] dp=new int[m+1][n+1];
+      for(int i=0;i<=m;i++){
+        dp[i][0]=0;
+      }
+      for(int j=0;j<=n;j++){
+        dp[0][j]=0;
+      }
 
-        LCS(m-1,n-1,dp,text1,text2);
-        int max=0;
-
-        for(int[] i:dp){
-            for(int j:i){
-                if(j>max){
-                    max=j;
-                }
+      for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+            }else{
+                dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
             }
         }
+      } 
 
-        System.out.println(max);
-        return max;
+
+
+
+      return dp[m][n]; 
     }
 }
