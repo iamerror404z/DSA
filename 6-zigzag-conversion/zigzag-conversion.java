003f6a3e29801fbe0440ff80fb2ff0d09126d4d1
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-12-23 16:02:30 +0530

    Time: 4 ms (84.79%) | Memory: 46.4 MB (73.80%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,56 @@
+class Solution {
+
+    public int down(int index,StringBuilder[] sb,String s,int rows){
+        if(index>=s.length()){
+            return index;
+        }
+
+        int size=Math.min(rows,s.length()-index);
+
+        for(int i=0;i<size && index<s.length();i++){
+            sb[i].append(s.charAt(index++));
+        }
+
+        return index;
+    }
+
+    public int diagonal(int index,StringBuilder[] sb,String s,int rows){
+        if(index>=s.length()){
+            return index;
+        }
+
+        // int size=Math.min(1,);
+
+        for(int i=rows-2;i>=1 && index<s.length();i--){
+            sb[i].append(s.charAt(index++));
+        }
+
+
+        return index;
+    }
+
+    public String convert(String s, int numRows) {
+        int rows=numRows;
+        StringBuilder[] sb=new StringBuilder[rows];
+        
+        for(int i=0;i<rows;i++){
+            sb[i]=new StringBuilder();
+        }
+
+        int index=0;
+
+        while(index<s.length()){
+            index=down(index,sb,s,rows);
+            index=diagonal(index,sb,s,rows);
+        }
+
+        StringBuilder ans=new StringBuilder();
+
+        for(StringBuilder i:sb){
+            ans.append(i);
+        }
+        
+        return ans.toString();
+        
+    }
+}
