a067ed980f14466e442161a5b0851299c0f88685
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-01-30 23:17:10 +0530

    Time: 1 ms (80.39%) | Memory: 43.1 MB (35.70%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1=version1.split("\\.");
+        String[] v2=version2.split("\\.");
+
+        int pointer1=0;
+        int pointer2=0;
+
+        while(pointer1<v1.length && pointer2<v2.length){
+            int num1=Integer.parseInt(v1[pointer1++]);
+            int num2=Integer.parseInt(v2[pointer2++]);
+
+            if(num1<num2){
+                return -1;
+            }
+
+            if(num1>num2){
+                return 1;
+            }
+
+        }
+
+        while(pointer1<v1.length){
+            int num1=Integer.parseInt(v1[pointer1++]);
+            
+            if(num1>0){
+                return 1;
+            }
+        }
+
+
+        while(pointer2<v2.length){
+            int num2=Integer.parseInt(v2[pointer2++]);
+
+            if(num2>0){
+                return -1;
+            }
+        }
+
+
+
+        System.out.println(Arrays.toString(v1));
+
+        
+
+
+        return 0;
+    }
+}
