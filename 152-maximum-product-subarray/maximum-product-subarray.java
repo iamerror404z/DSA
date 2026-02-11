6d3656d06b8236ec5f95773017d5024a3023483b
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-02-11 16:55:08 +0530

    Time: 1 ms (90.87%) | Memory: 47.5 MB (12.69%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int length=nums.length;
+        long[] track=new long[length];
+        long product=1;
+        int start=0;
+
+        for(int i=0;i<length;i++){
+            product*=nums[i];
+            track[i]=product;
+            if(nums[i]==0){
+                start=i+1;
+                product=1;
+            }
+        }
+
+
+        int[] negative=new int[length+1];
+        negative[length]=1;
+        int prev=length;
+
+        for(int i=length-1;i>=0;i--){
+            if(nums[i]<0){
+                prev=i;
+            }
+            negative[i]=prev;
+        }
+
+        // System.out.println(Arrays.toString(track));
+        // System.out.println(Arrays.toString(negative));
+
+        int max=(int)track[0];
+        start=0;
+
+        for(int i=0;i<length;i++){
+            if(track[i]==0){
+                start=i+1;
+                max=Math.max(0,max);
+            }else if(track[i]>0){
+                max=Math.max(max,(int)track[i]);
+            }else{
+                int index=negative[start];
+                long tempMax=track[i];
+                // System.out.println("i is :"+i+" to cut till : "+index);
+
+                if(index<i){
+                    tempMax/=track[index];
+                    // System.out.println(tempMax);
+                    max=Math.max(max,(int)tempMax);
+                }
+
+            }
+        }
+
+
+        
+
+        return max;
+    }
+}
