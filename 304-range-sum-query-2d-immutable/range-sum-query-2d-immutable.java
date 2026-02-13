84e515e8c8823e1f3ea9d4869f7041f43670a7be
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-02-13 18:06:44 +0530

    Time: 146 ms (12.76%) | Memory: 142.8 MB (7.84%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -0,0 +1,68 @@
+class NumMatrix {
+    int[][] forward;
+    int[][] vertical;
+
+    public NumMatrix(int[][] matrix) {
+        int rowSize=matrix.length;
+        int colSize=matrix[0].length;
+        forward=new int[rowSize][colSize];
+        int prev=0;
+
+        forward[0][0]=matrix[0][0];
+
+        for(int i=1;i<colSize;i++){
+            forward[0][i]=matrix[0][i]+forward[0][i-1];
+            System.out.println(forward[0][i]);
+        }
+
+
+
+        for(int i=1;i<rowSize;i++){
+            forward[i][0]=forward[i-1][0]+matrix[i][0];
+            for(int j=1;j<colSize;j++){
+                int subtract=forward[i-1][j-1];
+                forward[i][j]=forward[i][j-1]+forward[i-1][j]+matrix[i][j]-subtract;
+            }
+        }
+
+        for(int i[]:forward){
+            System.out.println(Arrays.toString(i));
+        }
+        
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans=forward[row2][col2];
+
+        int left=0;
+
+        if(col1>0){
+            int col=col1-1;
+            int row=row2;
+            left=forward[row][col];
+        }
+
+
+        int top=0;
+
+        if(row1>0){
+            int row=row1-1;
+            top=forward[row][col2];
+
+            if(col1>0){
+                top-=forward[row][col1-1];
+            }
+
+        }
+        
+        ans-=(top+left);
+
+        return ans;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
