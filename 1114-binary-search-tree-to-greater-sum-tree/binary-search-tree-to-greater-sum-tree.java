b0dc1281d9534f1608488d63dd3962987b030c2b
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-04-15 22:18:18 +0530

    Time: 8 ms (1.37%) | Memory: 43.2 MB (24.91%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int dfs(TreeNode curr,int above){
+        if(curr==null){
+            return 0;
+        }
+
+        if(curr.left==null && curr.right==null){
+            if(above!=0){
+                curr.val+=above;
+            }
+
+            return curr.val;
+        }
+        int rightDown=dfs(curr.right,above);
+
+        if(above!=0 && curr.right!=null){
+            System.out.println("above is : "+above);
+            rightDown=(rightDown-above);
+            System.out.println("rightdown is : "+rightDown);
+            
+        }
+
+
+        curr.val+=rightDown+above;
+
+        int leftDown=dfs(curr.left,curr.val);
+
+        if(curr.left!=null){
+            leftDown-=curr.val;
+        }
+        
+
+
+
+        return curr.val+leftDown;
+    }
+
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root,0);
+
+        return root;
+    }
+}
