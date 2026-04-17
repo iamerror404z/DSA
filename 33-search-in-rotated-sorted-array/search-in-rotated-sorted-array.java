891512918f2b611215f4be02232d7c2e730dccf1
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-04-17 13:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (69.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,48 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
-            }}
-             return -1;
 
+    public int binarySearch(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+
+            if(arr[mid]==target){
+                return mid;
+            }
+
+
+            if(arr[start]<=arr[mid]){
+                if(target>=arr[start] && target<=arr[mid]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else{
+                if(target>=arr[mid] && target<=arr[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;;
+                }
+            }
+        }
+
+            
+
+
+
+            return -1;
+        
+    }
+
+
+    public int search(int[] nums, int target) {
+        int index=binarySearch(nums,target);
+
+        // System.out.println(index);
+
+
+        return index;
     }
 }
