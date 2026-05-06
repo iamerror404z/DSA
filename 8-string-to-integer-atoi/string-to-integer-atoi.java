c24a680c9735a327b73b12b4f62b9f85606f4e52
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-05-06 20:12:33 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (87.83%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,80 @@
+class Solution {
+    
+    public long findNum(int start,String s){
+        long number=(long)Math.pow(10,16);
+        int length=s.length();
+        
+        while(start<length && s.charAt(start)=='0'){
+            start++;
+        }
+        
+        if(start>=length){
+            return 0;
+        }
+        
+        long  curr=(long)Math.pow(10,15);
+        long prev=curr;
+        
+        
+        for(int i=start;i<length;i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+            long next=curr/10;
+            prev=curr;
+           
+            long digit=curr*(s.charAt(i)-'0');
+            number+=digit;
+            
+            curr=next;
+            }else{
+                break;
+            }            
+        }
+        
+        long newNum=number-(long)Math.pow(10,16);
+        
+        
+        if(prev<=1){
+            return newNum;
+        }
+       
+        return newNum/prev;
+    }
+    
+    
+    
+    public int myAtoi(String s) {
+        long sign=1;
+        int index=0;
+        
+        int length=s.length();
+        
+        while(index<length && s.charAt(index)==' '){
+            index++;
+        }
+        
+        if(index>=length){
+            return 0;
+        }else if(s.charAt(index)=='-'){
+            sign=-1;
+            index++;
+        }else if(s.charAt(index)=='+'){
+            index++;
+        }
+        
+        long num=findNum(index,s);
+        
+        num*=sign;
+        
+        if(num<Integer.MIN_VALUE){
+            return -1*(int)Math.pow(2,31)-1;
+        }else if(num>Integer.MAX_VALUE){
+            return (int)Math.pow(2,31);
+        }
+        
+        
+        
+        
+        
+        return (int)num;
+    }
+}
