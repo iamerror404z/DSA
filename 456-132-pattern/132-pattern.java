a581c4ff7407bfccb9a8a51a2f88174782c0eeae
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-05-13 20:49:16 +0530

    Time: 270 ms (5.03%) | Memory: 109.4 MB (5.67%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,123 @@
+class Solution {
+
+    public boolean is132(int left,int curr,int right){
+        if(left<right && right<curr){
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean sort(int[][] arr,int start,int mid,int end){
+        int size=end-start+1;
+        int[][] temp=new int[size][2];
+        boolean flag=false;
+
+        int pointer1=start;
+        int pointer2=mid+1;
+        int index=0;
+
+        while(pointer1<=mid && pointer2<=end){
+            if(arr[pointer1][0]>arr[pointer2][0]){
+                temp[index][0]=arr[pointer1][0];
+                temp[index][1]=arr[pointer1][1];
+
+                flag=flag || is132(temp[index][1],temp[index][0],arr[pointer2][0]);
+
+                // if(flag){
+                //     System.out.println(temp[index][0]);
+                // }
+
+                pointer1++;
+                index++;
+            }else{
+                temp[index][0]=arr[pointer2][0];
+                temp[index][1]=arr[pointer2][1];
+                pointer2++;
+                index++;
+            }
+        }
+
+        while(pointer1<=mid){
+            temp[index][0]=arr[pointer1][0];
+                temp[index][1]=arr[pointer1][1];
+
+                flag=flag || is132(temp[index][1],temp[index][0],arr[mid+1][0]);
+                
+                // if(temp[index][0]==-1){
+                //     System.out.println("nums is : "+temp[index][0]);
+                //     System.out.println("left is : "+temp[index][1]);
+                //     System.out.println("right is : "+arr[end][0]);
+                // }
+
+                // if(flag){
+                //     System.out.println("num is "+temp[index][0]);
+                //     System.out.println("arr[end] is : "+arr[end][0]+"\n\n");
+                // }
+
+                pointer1++;
+                index++;
+        }
+
+        while(pointer2<=end){
+            temp[index][0]=arr[pointer2][0];
+            temp[index][1]=arr[pointer2][1];
+            pointer2++;
+            index++;
+        }
+
+
+        for(int i=0;i<size;i++){
+            arr[start+i][0]=temp[i][0];
+            arr[start+i][1]=temp[i][1];
+        }
+
+
+
+
+        return flag;
+    }
+
+    public boolean mergeSort(int[][] arr,int start,int end){
+        if(start>=end){
+            return false;
+        }
+
+        int mid=start+(end-start)/2;
+
+        boolean right=mergeSort(arr,mid+1,end);
+        boolean left=mergeSort(arr,start,mid);
+
+        boolean curr=sort(arr,start,mid,end);
+
+
+        return right || curr || left;
+    }
+
+
+    public boolean find132pattern(int[] nums) {
+        int size=nums.length;
+        int[][] arr=new int[size][2];
+        arr[0][0]=nums[0];
+        arr[0][1]=Integer.MAX_VALUE;
+
+        for(int i=1;i<size;i++){
+            arr[i][0]=nums[i];
+            arr[i][1]=Math.min(arr[i-1][1],nums[i-1]);
+        }
+
+       boolean  flag= mergeSort(arr,0,size-1);
+
+
+        // for(int[] i:arr){
+        //     System.out.print(Arrays.toString(i)+", ");
+        // }
+
+
+
+
+
+        return flag;
+
+    }
+}
