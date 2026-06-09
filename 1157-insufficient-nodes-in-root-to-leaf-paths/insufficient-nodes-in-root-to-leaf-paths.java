8dce233819414dca70d150ab0be78370d02a6931
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-06-09 22:03:05 +0530

    Time: 1 ms (34.44%) | Memory: 47 MB (86.11%) - LeetSync

---
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.java
@@ -0,0 +1,99 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+    res[0]=maxSum;
+    
+    res[1]:
+        0==>null by default
+        1==>null by decision
+    */
+
+
+
+    public long[] dfs(TreeNode curr,int limit,long runSum){
+        long[] res=new long[3];
+
+       if(curr==null){
+        return res;
+       }
+
+       long max=Long.MIN_VALUE;
+       boolean notFlag=true;
+
+       long[] left=dfs(curr.left,limit,runSum+curr.val);
+       long[] right=dfs(curr.right,limit,runSum+curr.val);
+
+       if(left[1]==1){
+        curr.left=null;
+       }
+
+       if(left[1]!=0){
+        notFlag=false;
+        max=Math.max(max,left[0]);
+       }
+
+
+       if(right[1]==1){
+        curr.right=null;
+       }
+
+       
+
+       if(right[1]!=0){
+        notFlag=false;
+        max=Math.max(max,right[0]);
+       }
+
+
+       if(notFlag){
+        max=0;
+       }
+
+
+       max+=curr.val+runSum;
+
+       if(max<limit){
+        res[1]=1;
+       }else{
+        res[1]=2;
+       }
+       
+       res[0]=max-runSum;
+
+
+      
+      return res;
+    }
+
+
+
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+       long[] res=dfs(root,limit,0);
+
+       if(res[1]==1){
+        return null;
+       }
+
+
+    //    root.val=147;
+
+    //    root.right=new TreeNode(147);
+
+
+        return root;
+    }
+}
