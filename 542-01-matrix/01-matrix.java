49729043024fb9c79020e7d24ed91137fabe1c97
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-06-11 20:53:24 +0530

    Time: 23 ms (5.22%) | Memory: 50.5 MB (5.06%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         Queue<int[]> queue=new LinkedList<>();
         int rowSize=mat.length;
         int colSize=mat[0].length;
-        int[][] visited=new int[rowSize][colSize];
         int[][] res=new int[rowSize][colSize];
        
 
@@ -19,13 +18,11 @@ class Solution {
                 int left=(j-1)>=0?mat[i][j-1]:max;
                 int right=(j+1)<colSize?mat[i][j+1]:max;
                 
-                if(curr==0){
-                    visited[i][j]=1;
-                }
                 
                 min=Math.min(min,Math.min(left,right));
 
                 if(min==0 && curr==1){
+                    res[i][j]=1;
 
 
                     queue.add(new int[]{i,j});
@@ -34,69 +31,51 @@ class Solution {
         }
 
 
-
-
-
-
-
-
-
-
         while(!queue.isEmpty()){
             int[] curr=queue.poll();
             int row=curr[0];
             int col=curr[1];
 
-            if(visited[row][col]==1){
+            if(mat[row][col]==0){
                 continue;
             }
 
             
-            visited[row][col]=1;
-
-            
-            int max=Integer.MAX_VALUE-10;
+            mat[row][col]=0;
+            int max=Integer.MAX_VALUE;
             int min=max;
             int up=max,down=max,left=max,right=max;
 
             if(row-1>=0){
-                if(visited[row-1][col]==0){
+                if(mat[row-1][col]==1){
                     queue.add(new int[]{row-1,col});
                 }else{
                     min=res[row-1][col];
-                    up=min;
                 }
             }
             if(row+1<rowSize){
-                if(visited[row+1][col]==0){
+                if(mat[row+1][col]==1){
                     queue.add(new int[]{row+1,col});
                 }else{
                     min=Math.min(min,res[row+1][col]);
-                    down=res[row+1][col];
                 }
             }
 
             if(col-1>=0){
-                if(visited[row][col-1]==0){
+                if(mat[row][col-1]==1){
                     queue.add(new int[]{row,col-1});
                 }else{
                     min=Math.min(min,res[row][col-1]);
-                    left=res[row][col-1];
                 }
             }
 
-
             if(col+1<colSize){
-                if(visited[row][col+1]==0){
+                if(mat[row][col+1]==1){
                     queue.add(new int[]{row,col+1});
                 }else{
                     min=Math.min(min,res[row][col+1]);
-                    right=res[row][col+1];
                 }
             }
-
-
-            
 
             res[row][col]=min+1;
 
@@ -104,8 +83,9 @@ class Solution {
 
         }
 
-
-
+        // for(int i[]:res){
+        //     System.out.println(Arrays.toString(i));
+        // }
 
 
 
