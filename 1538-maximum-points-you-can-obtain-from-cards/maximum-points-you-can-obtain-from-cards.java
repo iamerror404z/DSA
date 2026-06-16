132337e21ca701211c8a5992775767c199834b18
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-06-16 21:39:37 +0530

    Time: 65 ms (3.07%) | Memory: 68.5 MB (5.09%) - LeetSync

---
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int size=cardPoints.length;
+        int[] postSum=new int[size];
+        int[] preSum=new int[size];
+        int end=size-1;
+
+        preSum[0]=cardPoints[0];
+        postSum[end]=cardPoints[end];
+
+
+        for(int i=1;i<size-1;i++){
+            preSum[i]=preSum[i-1]+cardPoints[i];
+            postSum[end-i]=postSum[end-i+1]+cardPoints[end-i];
+
+        }
+
+        preSum[end]=preSum[end-1]+cardPoints[end];
+
+
+        if(end>=1){
+        postSum[0]=postSum[1]+cardPoints[0];}
+
+        int max=0;
+
+        System.out.println(Arrays.toString(preSum));
+        System.out.println(Arrays.toString(postSum));
+
+
+        int startPointer=0;
+        int endPointer=end;
+
+        for(int i=1;i<=k;i++){
+            int rem=k-i;
+            int leftSum=cardPoints[startPointer]-preSum[startPointer]+preSum[Math.min(end,startPointer+rem)];
+            int rightSum=cardPoints[endPointer]-postSum[endPointer]+postSum[Math.max(0,endPointer-rem)];
+
+
+            if(leftSum>=rightSum){
+                max+=cardPoints[startPointer];
+                startPointer++;
+            }
+            else{
+                max+=cardPoints[endPointer];
+                endPointer--;
+            }
+            
+                // System.out.println(max);
+        }
+
+
+
+        return max;
+        
+    }
+}
