b4c180c2f1501479eed2161fdd88986c49d6e4b3
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-06-21 16:37:01 +0530

    Time: 21 ms (11.11%) | Memory: 47.5 MB (40.61%) - LeetSync

---
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -0,0 +1,77 @@
+class Solution {
+
+    public void adder(int row,int col,int distance,Queue<int[]> queue){
+        queue.add(new int[]{row,col,distance});
+    }
+
+
+    public void validator(int row,int col,int distance,Queue<int[]> queue,int[][] grid,int size){
+        boolean status=(row>=0 && row<size) && (col>=0 && col<size) && (grid[row][col]==0);
+        
+        if(!status){
+            return ;
+        }
+
+        adder(row,col,distance,queue);
+    }
+
+    public int maxDistance(int[][] grid) {
+        Queue<int[]> queue=new LinkedList<>();
+        int size=grid.length;
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                
+                if(grid[i][j]==1){
+                    validator(i-1,j,2,queue,grid,size);
+                    validator(i+1,j,2,queue,grid,size);
+                    validator(i,j-1,2,queue,grid,size);
+                    validator(i,j+1,2,queue,grid,size);
+                   
+                }
+            }
+        }
+
+        // int qSize=queue.size();
+
+        // for(int i=0;i<qSize;i++){
+        //     int[] curr=queue.poll();
+        //     System.out.println(Arrays.toString(curr));
+        //     queue.add(curr);
+        // }
+
+        int max=0;
+
+
+        // System.out.println(queue);
+
+        while(!queue.isEmpty()){
+            int curr[]=queue.poll();
+            int i=curr[0];
+            int j=curr[1];
+            int distance=curr[2];
+
+            if(grid[i][j]!=0){
+                continue;
+            }
+            
+            max=Math.max(max,distance);
+
+
+            grid[i][j]=1;
+            distance++;
+
+            validator(i-1,j,distance,queue,grid,size);
+            validator(i+1,j,distance,queue,grid,size);
+            validator(i,j-1,distance,queue,grid,size);
+            validator(i,j+1,distance,queue,grid,size);
+            
+        }
+
+
+
+
+
+        return max-1;
+    }
+}
