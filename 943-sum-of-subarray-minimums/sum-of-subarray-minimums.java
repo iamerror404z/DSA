a0ec7626a11a4862f090b5b16e1ec98b78238411
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-07-25 14:49:34 +0530

    Time: 82 ms (69.42%) | Memory: 59.8 MB (80.90%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,86 @@
+class Solution {
+
+
+    public void  process(Deque<Integer> deque,int prev,int[] arr,int[] sum){
+        int endIndex=deque.peekLast();
+        int mod=sum[1];
+        
+
+
+        while(!deque.isEmpty() && prev<arr[deque.peekLast()]){
+            int currIndex=deque.pollLast();
+            int prevIndex=deque.isEmpty()?-1:deque.peekLast();
+
+            // finding the total number of times per a cycle
+            long cycle=1;
+
+            long cycleLength=endIndex-currIndex+1;
+            cycle=cycleLength*arr[currIndex];
+            // we are done with the sum per a cycle 
+
+
+            int numOfCycles=currIndex-prevIndex;
+            long ans=((numOfCycles)*cycle)%mod;
+            ans+=sum[0];
+
+            sum[0]=(int)(ans%mod);
+
+            // long totalContribution=contribution*times;
+
+            // System.out.println("c is : "+contribution);
+
+            // long tempSum=(contribution+sum[0]);
+            // sum[0]=(int)(tempSum%sum[1]);
+
+
+        }
+
+       
+
+        
+
+    }
+
+
+
+    public int sumSubarrayMins(int[] arr) {
+        final int mod=(int)Math.pow(10,9)+7;
+        int[] sum=new int[2];
+        sum[1]=mod;
+
+        Deque<Integer> deque=new LinkedList<>(); 
+        
+        
+        deque.addLast(0);
+        int size=arr.length;
+
+
+        for(int i=1;i<size;i++){
+            int prev=arr[deque.peekLast()];
+            
+            int curr=arr[i];
+
+            if(curr<prev){
+                process(deque,arr[i],arr,sum);
+                deque.add(i);
+            }else{
+                deque.add(i);
+            }
+
+
+        }
+
+
+       
+        System.out.println(deque);
+
+        process(deque,-1,arr,sum);
+
+
+
+
+
+        return sum[0];
+        
+    }
+}
