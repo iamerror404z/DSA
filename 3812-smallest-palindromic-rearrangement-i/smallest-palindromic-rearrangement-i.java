d8fea6afda7aa22e23bfc0abcd5462c8f14d35f5
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-07-28 10:43:39 +0530

    Time: 19 ms (97.19%) | Memory: 48.3 MB (38.15%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,51 +1,18 @@
-
 class Solution {
     public String smallestPalindrome(String s) {
-        int length=s.length();
-        int[] freq=new int[26];
-        char[] chars=s.toCharArray();
+        int n = s.length(), j = 0;
+        int[] freq = new int[26];
+        char[] chars = s.toCharArray();
 
-        for(char ch:chars){
-            freq[ch-'a']++;
-        }
+        for (int i = 0; i < n >> 1; i++)
+            freq[(chars[i] & 31) - 1]++;
 
-        int start=0;
-        int end=length-1;
-        
-        int mid=-1;
-
-        System.out.println(Arrays.toString(freq));
-
-
-        for(int i=0;i<26;i++){
-            char curr=(char)('a'+i);
-
-            int times=freq[i];
-
-
-            while(times>=2){
-                chars[start]=curr;
-                chars[end]=curr;
-                start++;
-                end--;
-                times-=2;
-
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]-- > 0) {
+                chars[j] = (char) (97 + i);
+                chars[n - 1 - j++] = (char) (97 + i);
             }
-            
-            if(times!=0){
-                mid =i;
-            }
-
         }
-        
-        if(mid!=-1){
-            char curr=(char)('a'+mid);
-            
-            chars[length/2]=curr;
-        }
-        
-        
-        
 
         return new String(chars);
     }
