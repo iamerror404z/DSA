482945fac615af6fb293938e9c29375921e4732b
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-07-31 10:14:20 +0530

    Time: 6 ms (30.40%) | Memory: 46.4 MB (32.89%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numSub(String s) {
+        int length=s.length();
+        int mod=(int)Math.pow(10,9)+7;
+        
+        
+        int count=0;
+        int start=0;
+        for(int i=0;i<=length;i++){
+            char curr=i==length?'0':s.charAt(i);
+            
+            if(curr=='0'){
+                long windowSize=i-start;
+                
+                long windowSum=windowSize*(windowSize+1)/2;
+                // System.out.println
+                
+                windowSum%=mod;
+                count=(int)((count+windowSum)%mod);
+                
+                start=i+1;
+            }
+            
+            
+        }
+        
+        
+        return count;
+    }
+}
