2ff8261605ae47dc83fb65c99f00c22c58a941c2
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-01 22:03:08 +0530

    Time: 2 ms (27.94%) | Memory: 45.5 MB (33.00%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,80 @@
+class Solution {
+
+
+    public int numOfDigits(int num){
+        if(num<10){
+            return 1;
+        }else if(num<100){
+            return 2;
+        }else if(num<1000){
+            return 3;
+        }
+
+        return 4;
+}
+
+
+    public void updateChars(char[] chars,char curr,int[] index,int freq){
+        chars[index[0]++]=curr;
+
+        if(freq==1){
+            return ;
+        }
+
+        int size=numOfDigits(freq);
+        
+        
+        index[0]+=size-1;
+
+
+        for(int i=0;i<size;i++){
+            int d=freq%10;
+
+            char currD=(char)(48+d);
+            chars[index[0]-i]=currD;
+
+
+            freq/=10;
+        }
+        index[0]++;
+        
+
+        return;
+        
+
+
+        
+    }
+   
+   public int compress(char[] chars) {
+        char prev=chars[0];
+
+        int[] end=new int[1];
+        int freq=1;
+        
+
+        for(int i=1;i<chars.length;i++){
+           char curr=chars[i];
+
+           if(curr!=prev){
+            updateChars(chars,prev,end,freq);
+
+            freq=0;
+           }
+           
+           freq++;
+            prev=curr;
+        }
+
+        updateChars(chars,prev,end,freq);
+
+
+        String res=Arrays.toString(chars);
+   
+
+
+
+
+        return end[0];
+    }
+}
