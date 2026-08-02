fe4fd69b32e223ed1e5e8b728db90f30d4b6ab0c
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-02 21:55:11 +0530

    Time: 904 ms (6.11%) | Memory: 108.2 MB (99.83%) - LeetSync

---
+++ 4203-count-of-unfinished-tasks-after-each-shift/count-of-unfinished-tasks-after-each-shift.java
@@ -0,0 +1,77 @@
+class Solution {
+
+    public int biSearch(int start,long sum,long[] prefixSum){
+        int end=prefixSum.length-1;
+        int pos=0;
+        
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(prefixSum[mid]<=sum){
+                pos=mid;
+                start=mid+1;
+            }else{
+                end=mid-1;
+
+            }
+
+
+
+        }
+
+
+        return pos;
+
+    }
+
+
+
+    public int[] countTasks(int[] tasks, int[] shifts) {
+        int length=shifts.length;
+        long[] prefixSum=new long[tasks.length];
+
+        prefixSum[0]=tasks[0];
+        for(int i=1;i<tasks.length;i++){
+            prefixSum[i]=tasks[i]+prefixSum[i-1];
+        }
+
+
+        // System.out.println(Arrays.toString(prefixSum));
+
+
+        long sum=0;
+        int start=0;
+        int[] res=new int[length];
+
+        for(int i=0;i<shifts.length;i++){
+            int curr=shifts[i];
+            sum+=curr;
+
+            start=biSearch(start,sum,prefixSum);
+            int remTasks=tasks.length-1-start;
+            
+            if(prefixSum[start]>sum){
+                remTasks=tasks.length;
+            }
+            
+
+            // System.out.println("sum is :"+sum);
+            // System.out.println("start index is : "+start);
+            System.out.println("prefixSum is : "+prefixSum[start]);
+
+
+            if(start==tasks.length-1){
+                sum=0;
+                start=0;
+            }
+                res[i]=remTasks;
+        }
+
+
+
+
+        return res;
+        
+    }
+}
