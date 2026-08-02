5880d27641602e79e11dd2bb57aa68cd96982395
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-02 21:21:34 +0530

    Time: 21 ms (47.53%) | Memory: 47 MB (92.86%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb=new StringBuilder();
+        Deque<Integer> deque=new LinkedList();
+
+
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+
+
+            if(curr=='('){
+                deque.addLast(i);
+            }else if(curr==')'){
+                if(!deque.isEmpty() && s.charAt(deque.peekLast())=='('){
+                deque.pollLast();
+                }else{
+                    deque.addLast(i);
+                }
+            
+         }
+
+        }
+
+         System.out.println(deque);
+
+         for(int i=0;i<s.length();i++){
+            if(!deque.isEmpty() && i==deque.peekFirst()){
+                deque.pollFirst();
+                continue;
+            }
+
+            sb.append(s.charAt(i));
+         }
+
+
+        
+
+        return sb.toString();
+    }
+}
