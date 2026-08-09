b3f91328c9bdc849fe8181d21d04d1aa1bba0260
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-09 17:34:56 +0530

    Time: 1939 ms (5.24%) | Memory: 47.6 MB (5.61%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,68 @@
+class Solution {
+
+    public int dp(int start,int m,int sum,int[] piles,int[][] memo){
+        int end=start+(2*m);
+
+        // base case
+      
+        if(end>=piles.length){
+              int currSum=0; 
+            for(int i=start;i<piles.length;i++){
+                currSum+=piles[i];
+            }
+
+            return currSum;
+
+        }
+
+        if(memo[start][m]!=-1){
+            return memo[start][m];
+        }
+
+        int currSum=0;
+        int max=0;
+
+        
+            System.out.println("start is : "+start);
+            System.out.println("m is : "+m);
+
+        for(int i=start;i<end;i++){
+            currSum+=piles[i];
+
+            int player2=dp(i+1,Math.max(i-start+1,m),sum-currSum,piles,memo);
+            int nextSum=sum-(currSum+player2);
+
+            System.out.println("currSum is : "+currSum);
+            System.out.println("nextSum is "+nextSum);
+            System.out.println("player2 is :"+player2);
+
+            max=Math.max(max,currSum+nextSum);
+        }
+
+
+
+        memo[start][m]=max;
+
+        return max;
+    }
+
+
+
+    public int stoneGameII(int[] piles) {
+        int n=piles.length;
+
+        int[][] memo=new int[n][n];
+
+        for(int i[]:memo){
+            Arrays.fill(i,-1);
+        }
+        int sum=0;
+
+        for(int i:piles){
+            sum+=i;
+        }
+
+
+        return dp(0,1,sum,piles,memo);
+    }
+}
