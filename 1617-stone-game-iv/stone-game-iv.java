09356fcf57de1958da4a30a15b43736da9be0edd
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-10 12:19:05 +0530

    Time: 152 ms (8.62%) | Memory: 50.8 MB (38.12%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public int dp(int n,int[] dp){
+        if(n<=0){
+            return 0;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        int win=0;
+
+        for(int i=1;i*i<=n;i++){
+            int nextPlayer=dp(n-(i*i),dp);
+            
+            int currPlayer=1^nextPlayer;
+
+            if(currPlayer==1){
+                dp[n]=1;
+                return 1;
+            }
+
+
+        }
+
+
+
+        return win;
+    }
+
+
+
+    public boolean winnerSquareGame(int n) {
+        int[] memo=new int[n+1];
+
+       Arrays.fill(memo,-1);
+
+        int res=dp(n,memo);
+        
+
+
+        return res==1;
+    }
+}
