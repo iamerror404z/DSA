4ad96140c9c2f1b348648022132f3bbdd5c0992d
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-13 14:06:56 +0530

    Time: 8 ms (5.23%) | Memory: 86.6 MB (80.48%) - LeetSync

---
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int minPos=0;
+        int maxPos=0;
+
+        int length=nums.length;
+        for(int i=0;i<length;i++){
+            int curr=nums[i];
+
+            if(curr<nums[minPos]){
+                minPos=i;
+            }
+
+            if(curr>nums[maxPos]){
+                maxPos=i;
+            }
+        }
+
+        int left=Math.min(minPos,maxPos);
+        int right=Math.max(maxPos,minPos);
+
+
+        int minDeletions=length;
+
+        int leftDistance=0;
+        int rightDistance=0;
+        
+        // poss1 delete it from both the end
+        leftDistance=left+1;
+        rightDistance=length-right;
+        int poss1=leftDistance+rightDistance;
+
+        // poss2 delete the left and remove the element from left to the right 
+        int poss2=(right+1);
+      
+
+        // poss3 delete elements from the right most to left most;
+        int poss3=length-(left);
+        int min=Math.min(poss1,poss2);
+        min=Math.min(min,poss3);
+
+
+        System.out.println("left is : "+left);
+        System.out.println("right is : "+right);
+        System.out.println("min is : "+min);
+        return Math.min(Math.min(poss1,poss2),poss3);
+    }
+}
