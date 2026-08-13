ed44164c8a2c6ff4c912e7bca0386bdcc6a5c5d8
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2026-08-13 11:43:04 +0530

    Time: 258 ms (5.04%) | Memory: 101.8 MB (8.40%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    public boolean isPossible(int[] dist,double hour,int speed){
+        int ceil=(int)Math.ceil(1.56478);
+        double required=0;
+        System.out.println("ceil is : "+ceil);
+
+
+        for(int i=0;i<dist.length;i++){
+            required=Math.ceil(required);
+            int curr=dist[i];
+
+            double currRequired=curr/(double)speed;
+            required+=currRequired;
+
+            
+            if(required>hour){
+                return false;
+            }
+        }
+
+
+
+
+        return true;
+    }
+
+
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int max=0;
+
+        for(int i:dist){
+            max=Math.max(max,i);
+        }
+
+
+        int speed=-1;
+
+        int start=1;
+        int end=(int)Math.pow(10,9);
+
+        // binary search
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(isPossible(dist,hour,mid)){
+                speed=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+
+        
+        System.out.println("res is : "+speed);
+        return speed;
+    }
+}
